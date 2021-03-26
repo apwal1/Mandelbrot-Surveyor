a0406e05f1db2eb597370d374ce07b70c9d4a547
apwal1 <66497996+apwal1@users.noreply.github.com>
2021-03-26 14:22:19 -0400

    Added SDL error checking
    GPU rendering probably coming in the next push

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -10,7 +10,7 @@
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
-/*Max iterations before a complex number is considered to not be in the mandelbrot set.
+/*Max iterations before a complex number is considered to be in the mandelbrot set.
 Increasing this will result in a better quality fractal with reduced performance.
 Reducing this will result in a lower quality fractal with improved performance.*/
 #define MAX_ITER 1024
@@ -19,6 +19,7 @@ Reducing this will result in a lower quality fractal with improved performance.*
 #define NUM_THREADS 32
 
 using std::complex;
+using std::string;
 using std::cout;
 using std::endl;
 using std::vector;
@@ -31,6 +32,7 @@ using namespace std::chrono;
 bool eventHandler(SDL_Event* event, bool* mousePanning, SDL_Point* mouseCoords, fracState* state);
 void zoom(fracState* state, double zoomAmount);
 void pan(int* beforeX, int* beforeY, const int* afterX, const int* afterY, fracState* state);
+void SDLError(string errorMsg, bool* errorFlag);
 
 int main(int argc, char* argv[]) {
     SDL_Event event;
@@ -46,6 +48,9 @@ int main(int argc, char* argv[]) {
 
     fracThread* threads[NUM_THREADS];
 
+    //This will be true if there was an error in the initialization of the SDL window/renderer
+    bool initError = false;
+
     //Creating a 2d array of ints
     int** result = new int* [WINDOW_WIDTH];
     for (int i = 0; i < WINDOW_WIDTH; i++)
@@ -60,58 +65,67 @@ int main(int argc, char* argv[]) {
         threads[i] = new fracThread(WINDOW_WIDTH, WINDOW_HEIGHT, MAX_ITER, bounds, result, &state);
     }
 
-
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
-
-    while (!eventHandler(&event, &mousePanning, &mousePanningCoords, &state))
+    //Initializes SDL video and checks for errors
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        SDLError("Error during SDL_Init", &initError);
+    //Initializes SDL window and checks for errors
+    if ((window = SDL_CreateWindow("Mandelbrot Surveyor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0)) == NULL)
+        SDLError("Error during SDL_CreateWindow", &initError);
+    //Initializes SDL renderer and checks for errors
+    if ((renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED)) == NULL)
+        SDLError("Error during SDL_CreateRenderer", &initError);
+    
+    if (!initError)
     {
-        auto start = high_resolution_clock::now();
+        while (!eventHandler(&event, &mousePanning, &mousePanningCoords, &state))
+        {
+            auto start = high_resolution_clock::now();
 
-        //Starts the threads
-        for (auto& i : threads)
-            i->run();
+            //Starts the threads
+            for (auto& i : threads)
+                i->run();
 
-        //Waits for every thread to finish its portion of the frame
-        for (auto& i : threads)
-            i->waitUntilDone();
+            //Waits for every thread to finish its portion of the frame
+            for (auto& i : threads)
+                i->waitUntilDone();
 
-        duration<double> time = high_resolution_clock::now() - start;
-        cout << "Created result array in " << time.count() << " seconds\n";
+            duration<double> time = high_resolution_clock::now() - start;
+            cout << "Created result array in " << time.count() << " seconds\n";
 
-        //Determines the color of each pixel and draws it to the screen
-        for (int x = 0; x < WINDOW_WIDTH; x++)
-            for (int y = 0; y < WINDOW_HEIGHT; y++)
-            {
-                if (result[x][y] == MAX_ITER)
-                    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-                else
+            //Determines the color of each pixel and draws it to the screen
+            for (int x = 0; x < WINDOW_WIDTH; x++)
+                for (int y = 0; y < WINDOW_HEIGHT; y++)
                 {
-                    //Makes the fractal look nice (color choosing)
-                    //These values were taken from https://stackoverflow.com/questions/16500656/which-color-gradient-is-used-to-color-mandelbrot-in-wikipedia
-                    switch (result[x][y] % 16)
+                    if (result[x][y] == MAX_ITER)
+                        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                    else
                     {
-                    case 0: SDL_SetRenderDrawColor(renderer, 66, 30, 15, 255); break;
-                    case 1: SDL_SetRenderDrawColor(renderer, 25, 7, 26, 255); break;
-                    case 2: SDL_SetRenderDrawColor(renderer, 9, 1, 47, 255); break;
-                    case 3: SDL_SetRenderDrawColor(renderer, 4, 4, 73, 255); break;
-                    case 4: SDL_SetRenderDrawColor(renderer, 0, 7, 100, 255); break;
-                    case 5: SDL_SetRenderDrawColor(renderer, 12, 44, 138, 255); break;
-                    case 6: SDL_SetRenderDrawColor(renderer, 24, 82, 177, 255); break;
-                    case 7: SDL_SetRenderDrawColor(renderer, 57, 125, 209, 255); break;
-                    case 8: SDL_SetRenderDrawColor(renderer, 134, 181, 229, 255); break;
-                    case 9: SDL_SetRenderDrawColor(renderer, 211, 236, 248, 255); break;
-                    case 10: SDL_SetRenderDrawColor(renderer, 241, 233, 191, 255); break;
-                    case 11: SDL_SetRenderDrawColor(renderer, 248, 201, 95, 255); break;
-                    case 12: SDL_SetRenderDrawColor(renderer, 255, 170, 0, 255); break;
-                    case 13: SDL_SetRenderDrawColor(renderer, 204, 128, 0, 255); break;
-                    case 14: SDL_SetRenderDrawColor(renderer, 153, 87, 0, 255); break;
-                    case 15: SDL_SetRenderDrawColor(renderer, 106, 52, 3, 255);
+                        //Makes the fractal look nice (color choosing)
+                        switch (result[x][y] % 16)
+                        {
+                            //These values were taken from https://stackoverflow.com/questions/16500656/which-color-gradient-is-used-to-color-mandelbrot-in-wikipedia
+                        case 0: SDL_SetRenderDrawColor(renderer, 66, 30, 15, 255); break;
+                        case 1: SDL_SetRenderDrawColor(renderer, 25, 7, 26, 255); break;
+                        case 2: SDL_SetRenderDrawColor(renderer, 9, 1, 47, 255); break;
+                        case 3: SDL_SetRenderDrawColor(renderer, 4, 4, 73, 255); break;
+                        case 4: SDL_SetRenderDrawColor(renderer, 0, 7, 100, 255); break;
+                        case 5: SDL_SetRenderDrawColor(renderer, 12, 44, 138, 255); break;
+                        case 6: SDL_SetRenderDrawColor(renderer, 24, 82, 177, 255); break;
+                        case 7: SDL_SetRenderDrawColor(renderer, 57, 125, 209, 255); break;
+                        case 8: SDL_SetRenderDrawColor(renderer, 134, 181, 229, 255); break;
+                        case 9: SDL_SetRenderDrawColor(renderer, 211, 236, 248, 255); break;
+                        case 10: SDL_SetRenderDrawColor(renderer, 241, 233, 191, 255); break;
+                        case 11: SDL_SetRenderDrawColor(renderer, 248, 201, 95, 255); break;
+                        case 12: SDL_SetRenderDrawColor(renderer, 255, 170, 0, 255); break;
+                        case 13: SDL_SetRenderDrawColor(renderer, 204, 128, 0, 255); break;
+                        case 14: SDL_SetRenderDrawColor(renderer, 153, 87, 0, 255); break;
+                        case 15: SDL_SetRenderDrawColor(renderer, 106, 52, 3, 255);
+                        }
                     }
+                    SDL_RenderDrawPoint(renderer, x, y);
                 }
-                SDL_RenderDrawPoint(renderer, x, y);
-            }
-        SDL_RenderPresent(renderer);
+            SDL_RenderPresent(renderer);
+        }
     }
 
     //Cleaning up SDL
@@ -220,4 +234,11 @@ bool eventHandler(SDL_Event* event, bool* mousePanning, SDL_Point* mouseCoords, 
     }
 
     return false;
+}
+
+//Logs an SDL error
+void SDLError(string errorMsg, bool* errorFlag)
+{
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, *errorMsg.c_str() + ": " + *SDL_GetError());
+    *errorFlag = true;
 }
